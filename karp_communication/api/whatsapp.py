36c7736b8d44462e840ceb0db0c2cbfaa329a16d
Sushhil-Pal <sushil.pal@gmail.com>
2024-09-18 23:40:00 +0530

    Refactoring and Adding Loyalty Points

--- karp_communication/api/whatsapp.py
+++ karp_communication/api/whatsapp.py
@@ -4,7 +4,42 @@ import frappe
 def get_wa_link(customer_name, wa_template_name):
 
     wa_url = "https://wa.me/";    
+    contact_doc = get_contact_for_customer(customer_name)
+    wa_url = wa_url + contact_doc.mobile_no
+    message = construct_message(wa_template_name, customer_name, contact_doc)
+    wa_url = wa_url + "?text=" + message
 
+    return wa_url
+
+
+def construct_message(wa_template_name, customer_name, contact_doc):
+
+    wa_template = frappe.get_doc("WA Template", {"name": wa_template_name})
+
+    context = build_context(wa_template_name, customer_name, contact_doc)
+
+    message = frappe.render_template(wa_template.message_template, context)
+
+    return message
+
+def build_context(wa_template_name, customer_name, contact_doc):
+
+    if(wa_template_name == "Thankyou Msg"):
+        loyalty_points = int(get_total_loyalty_points_for_customer(customer_name))
+        context = {
+            "first_name": contact_doc.first_name,
+            "loyalty_points": loyalty_points
+        }
+        return context
+        
+    else: 
+        context = {
+            "first_name": contact_doc.first_name
+        }
+        return context
+
+
+def get_contact_for_customer(customer_name):
     linked_contacts = frappe.get_all(
         'Dynamic Link',
         filters={
@@ -14,21 +49,23 @@ def get_wa_link(customer_name, wa_template_name):
         fields=['parent'],
         ignore_permissions=True
     )
-
     if linked_contacts:
         # Get the contact name from the result
         contact_name = linked_contacts[0]['parent']
+        contact_doc = frappe.get_doc('Contact', contact_name)
+        return contact_doc
+    else:
+        return None
 
-        # Fetch the contact details (phone, mobile_no)
-        contact_details = frappe.get_value(
-            'Contact',
-            contact_name,
-            ['mobile_no'],
-            as_dict=True
-        )
-        wa_template = frappe.get_doc("WA Template", {"name": wa_template_name})
-        wa_url = wa_url + contact_details.mobile_no
-        wa_url = wa_url + "?text=" + wa_template.message_template
+def get_total_loyalty_points_for_customer(customer_name):
+    query = """
+        SELECT SUM(loyalty_points) as total_loyalty_points
+        FROM `tabLoyalty Point Entry`
+        WHERE customer = %s
+    """ 
+    # Execute the query with customer_name as the parameter
+    result = frappe.db.sql(query, customer_name)
 
-        return wa_url
-    return None
+    # Return the total loyalty points, or 0 if no record is found
+    return result[0][0] if result and result[0][0] else 0
+
